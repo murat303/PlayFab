ca60784d7a15b15160314d90d0ac52997a503878
murat303 <murat303@gmail.com>
2020-11-08 00:29:32 +0300

    mobile login

--- Assets/Examples/Scripts/PlayFabLogin.cs
+++ Assets/Examples/Scripts/PlayFabLogin.cs
@@ -1,5 +1,6 @@
 ﻿using PlayFab;
 using PlayFab.ClientModels;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,22 +11,108 @@ public class PlayFabLogin : MonoBehaviour
     [SerializeField] InputField userPassword;
 
     [SerializeField] GameObject authPanel;
+    [SerializeField] GameObject userPanel;
     [SerializeField] Text txtUserName;
+
+    string email, pass;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("Email"))
+        {
+            email = PlayerPrefs.GetString("Email");
+            pass = PlayerPrefs.GetString("Pass");
+            if(!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(pass))
+                Login();
+        }
+        else
+        {
+#if UNITY_ANDROID
+            var requestAndroid = new LoginWithAndroidDeviceIDRequest { AndroidDeviceId = ReturnMobileID(), CreateAccount = true };
+            PlayFabClientAPI.LoginWithAndroidDeviceID(requestAndroid, OnLoginSuccessMobile, OnLoginFailure);
+#endif
+#if UNITY_IOS
+            var requestIOS = new LoginWithIOSDeviceIDRequest { DeviceId = ReturnMobileID(), CreateAccount = true };
+            PlayFabClientAPI.LoginWithIOSDeviceID(requestIOS, OnLoginSuccessMobile, OnLoginFailure);
+#endif
+        }
+    }
+
+    private string ReturnMobileID()
+    {
+        return SystemInfo.deviceUniqueIdentifier;
+    }
 
     public void BtnRegister()
     {
-        var request = new RegisterPlayFabUserRequest { Email = userEmail.text, Password = userPassword.text, Username = userName.text, DisplayName = userName.text };
+        email = userEmail.text;
+        pass = userPassword.text;
+        var request = new RegisterPlayFabUserRequest { Email = email, Password = pass, Username = userName.text, DisplayName = userName.text };
         PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnRegisterFailure);
     }
 
     public void BtnLogin()
     {
-        var request = new LoginWithEmailAddressRequest { Email = userEmail.text, Password = userPassword.text };
+        email = userEmail.text;
+        pass = userPassword.text;
+
+        Login();
+    }
+    public void BtnAddLogin() //For mobile recovery account
+    {
+        email = userEmail.text;
+        pass = userPassword.text;
+
+        var request = new AddUsernamePasswordRequest { Email = email, Password = pass, Username = userName.text };
+        PlayFabClientAPI.AddUsernamePassword(request, OnAddLoginSuccess, OnRegisterFailure);
+    }
+
+    public void BtnLogOut()
+    {
+        email = string.Empty;
+        pass = string.Empty;
+        PlayerPrefs.DeleteKey("Email");
+        PlayerPrefs.DeleteKey("Pass");
+        authPanel.SetActive(true);
+        userPanel.SetActive(false);
+        PlayFabClientAPI.ForgetAllCredentials();
+    }
+
+    void Login()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = email,
+            Password = pass,
+            InfoRequestParameters = new GetPlayerCombinedInfoRequestParams()
+            {
+                GetPlayerProfile = true,
+                ProfileConstraints = new PlayerProfileViewConstraints()
+                {
+                    ShowLastLogin = true,
+                    ShowDisplayName = true
+                }
+            }
+        };
         PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnLoginFailure);
     }
 
     void OnRegisterSuccess(RegisterPlayFabUserResult obj)
     {
+        SaveUserData();
+        authPanel.SetActive(false);
+        userPanel.SetActive(true);
+        txtUserName.text = obj.Username;
+
+        Debug.Log("Congratulations, you registered!");
+    }
+    void OnAddLoginSuccess(AddUsernamePasswordResult obj)
+    {
+        SaveUserData();
+        authPanel.SetActive(false);
+        userPanel.SetActive(true);
+        txtUserName.text = obj.Username;
+
         Debug.Log("Congratulations, you registered!");
     }
 
@@ -36,13 +123,32 @@ public class PlayFabLogin : MonoBehaviour
 
     void OnLoginSuccess(LoginResult result)
     {
+        SaveUserData();
         authPanel.SetActive(false);
-        txtUserName.text = result.InfoResultPayload.PlayerProfile.DisplayName; //TODO: neden null geliyor?
+        userPanel.SetActive(true);
+        txtUserName.text = 
+            "Username: " + result.InfoResultPayload.PlayerProfile.DisplayName + "\n" + 
+            "Last Login: " + result.InfoResultPayload.PlayerProfile.LastLogin.ToString();
+
+        Debug.Log("Congratulations, you made your first successful API call!");
+    }
+    void OnLoginSuccessMobile(LoginResult result)
+    {
+        authPanel.SetActive(false);
+        userPanel.SetActive(true);
+        txtUserName.text = "Welcome Android";
+
         Debug.Log("Congratulations, you made your first successful API call!");
     }
 
     void OnLoginFailure(PlayFabError error)
     {
         Debug.LogError(error.GenerateErrorReport());
+    }
+
+    void SaveUserData()
+    {
+        PlayerPrefs.SetString("Email", email);
+        PlayerPrefs.SetString("Pass", pass);
     }
 }
